0a5e7ba8930706b8861ac168992a66a6bfcee7bf
小滕 <xiaoteng@192.168.0.103>
2020-02-03 14:41:24 +0800

    mall

--- api/index.js
+++ api/index.js
@@ -2,10 +2,12 @@ import * as course from './course'
 import * as video from './video'
 import * as user from './user'
 import * as role from './role'
+import * as order from './order'
 
 module.exports = {
     course,
     video,
     user,
-    role
+    role,
+    order
 }
