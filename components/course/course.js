a2d9ed8c4dfecfb0c757aff18fa97990969d0936
小滕 <xiaoteng@192.168.0.103>
2020-07-22 16:52:31 +0800

    组件化

---
+++ components/course/course.js
@@ -0,0 +1,28 @@
+Component({
+  properties: {
+    name: {
+      type: String,
+      value: '',
+    },
+    thumb: {
+      type: String,
+      value: '',
+    },
+    userCount: {
+      type: Number,
+      value: 0,
+    },
+    category: {
+      type: String,
+      value: '',
+    },
+    charge: {
+      type: Number,
+      value: 0,
+    }
+  },
+  data: {
+  },
+  methods: {
+  }
+})
