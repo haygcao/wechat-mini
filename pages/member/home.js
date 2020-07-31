ee4f7071185aedd47e71cc7330c64415e8682a03
小滕 <xiaoteng@192.168.0.103>
2020-07-31 11:56:23 +0800

    fixed: tabbar激活状态

--- pages/member/home.js
+++ pages/member/home.js
@@ -29,6 +29,12 @@ Page({
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
+    if (typeof this.getTabBar === 'function' && this.getTabBar()) {
+      this.getTabBar().setData({
+        active: 'member'
+      })
+    }
+
     this.getUser();
   },
 
